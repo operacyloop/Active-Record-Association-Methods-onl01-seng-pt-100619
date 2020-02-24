742e1aae87ed544f8016bdaf0faab5669092ce3e
Barry Ivan Silber <age.ef.ess@gmail.com>
2020-02-24 14:09:39 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,12 +4,12 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
-    genre.songs.count
+    self.songs.count
   end
 
   def artist_count
     # return the number of artists associated with the genre
-    genre.artists.count
+    self.artists.count
   end
 
   def all_artist_names
