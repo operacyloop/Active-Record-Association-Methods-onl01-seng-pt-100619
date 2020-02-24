918aa92e8a1575ca3320aa09bec0abb616f7967e
Barry Ivan Silber <age.ef.ess@gmail.com>
2020-02-24 13:59:26 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,7 +14,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    @@all = []
+    # @@all = []
     # artists each do |artist|
     #   @@all << artist 
     # end
